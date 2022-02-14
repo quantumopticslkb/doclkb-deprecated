e590b2382290bc0127a123301ae5c04ac0baf575
Quentin Glorieux <quentin.glorieux@gmail.com>
2022-02-14 14:35:13 +0100

    Theming

--- theme-docs/src/components/app/AppFooter.vue
+++ theme-docs/src/components/app/AppFooter.vue
@@ -2,7 +2,6 @@
   <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20">
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
       <div class="flex items-end">
-        </a> -->
       </div>
       <div class="flex items-center space-x-4">
         <!-- <AppLangSwitcher />
